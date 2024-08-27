1c754141966097e79ba1c33766076c134cc517fb
Oliver Dienhart <odienhart@icloud.com>
2024-08-27 09:36:47 -0500

    fuxes

---
+++ College Board List/CollegeList.swift
@@ -0,0 +1,7 @@
+import SwiftUI
+
+struct CollegeList: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
---
+++ File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  College Board List
+//
+//  Created by Oliver W. Dienhart on 8/27/24.
+//
+
+import Foundation
